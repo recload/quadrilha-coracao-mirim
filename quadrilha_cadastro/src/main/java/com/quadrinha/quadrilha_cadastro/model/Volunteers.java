4c6adbd56b5ae06528b2abdb057424c645275638
Lucas Almeida <lucas.almeidasilva49@gmail.com>
2023-12-26 03:59:53 -0300

    Add constructor

--- quadrilha_cadastro/src/main/java/com/quadrinha/quadrilha_cadastro/model/Volunteers.java
+++ quadrilha_cadastro/src/main/java/com/quadrinha/quadrilha_cadastro/model/Volunteers.java
@@ -7,11 +7,13 @@ import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
 @AllArgsConstructor
+@RequiredArgsConstructor
 @Entity
 @Table(name = "volunteers")
 public class Volunteers {
